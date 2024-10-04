fd35604ba6b5ea65a391de3aa1f58f4bf90e683f
Salih <imago_999@hotmail.com>
2024-10-05 00:14:34 +0300

    Postgres database connected through IntellJ

---
+++ src/main/java/org/example/helpfullinkstostudycoding/HelpfulLinksToStudyCodingApplication.java
@@ -0,0 +1,13 @@
+package org.example.helpfullinkstostudycoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpfulLinksToStudyCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpfulLinksToStudyCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/helpfullinkstostudycoding/HelpfulLinksToStudyCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.helpfullinkstostudycoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpfulLinksToStudyCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
